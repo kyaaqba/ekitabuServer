5267119df905861e70cd896cedac1e306789674f
Karam Yaaqba <v-kayaaq@microsoft.com>
2020-06-08 23:28:06 -0700

    create db

--- script/installation.sh
+++ script/installation.sh
@@ -10,3 +10,4 @@ sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y apache2
+sudo apt-get install -y mysql-server
